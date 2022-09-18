0ff425bff8d9526421e60a220f8c43a801e546c4
luanntgenki <luan-nt@genkisystem.com>
2022-09-18 17:00:54 +0700

    Adding key for element

--- src/components/shared/mui/TabList.tsx
+++ src/components/shared/mui/TabList.tsx
@@ -60,8 +60,9 @@ const TabList = (props: Props) => {
     return (
         <TabsUnstyled className={classes.tabList} defaultValue={0}>
             <TabsListUnstyled>
-                {items.map(item => (
+                {items.map((item, key) => (
                     <TabUnstyled
+                        key={key}
                         onClick={() => onClick(item.value)}
                         style={{ backgroundColor: type == 'property' ? 'transparent' : '#fff' }}
                     >{item.label}</TabUnstyled>
