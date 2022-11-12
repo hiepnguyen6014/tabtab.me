b02682d6cf256aaa5e4c9ec0fa9200abf567151b
Cường Nguyễn Quốc <nguyenquoccuong1998x@gmail.com>
2022-11-12 15:01:52 +0700

    Update Filter UI match design

--- src/components/pages/Search/component/PriceContent/PriceContent.tsx
+++ src/components/pages/Search/component/PriceContent/PriceContent.tsx
@@ -1,8 +1,29 @@
-import { ButtonApply, ButtonClasses, ButtonClear, DivPrice } from '../../Search.style'
-import { Col, Divider, Dropdown, Menu, Row, Typography } from 'antd'
+import {
+  BoxContainer,
+  BoxDayWeek,
+  BoxDivider,
+  BoxDividerOne,
+  BoxTypographyPrice,
+  BoxTypographySelectType,
+  BoxWrapper,
+  BoxWrapperTwo,
+  BoxWrrapperRadius,
+  Container,
+  LabalOne,
+  Label,
+  Text,
+  TextOne,
+} from './PriceContentStyled';
+import {
+  ButtonApply,
+  ButtonClasses,
+  ButtonClear,
+  DivPrice,
+} from '../../Search.style';
+import { Col, Divider, Dropdown, Menu, Row, Typography } from 'antd';
 
 import { IconArrowDown } from '@root/public/icons';
-import React from 'react'
+import React from 'react';
 
 const PriceContent = () => {
   const menu = (
@@ -24,221 +45,81 @@ const PriceContent = () => {
     />
   );
   return (
-    <div style={{ padding: 12 }}>
-    <Typography
-      style={{ fontSize: 20, fontFamily: 'Inter', fontWeight: '700' }}
-    >
-      Price
-    </Typography>
-    <Typography
-      style={{
-        fontSize: 16,
-        fontFamily: 'Inter',
-        fontWeight: '700',
-        marginTop: 24,
-        marginBottom: 16,
-      }}
-    >
-      Select Type
-    </Typography>
-    <div
-      style={{
-        borderRadius: 8,
-        padding: 4,
-        display: 'flex',
-        alignItems: 'center',
-      }}
-    >
-      <div
-        style={{
-          background: '#E9E9E9',
-          padding: '4px 0px 4px 4px',
-          borderTopLeftRadius: 8,
-          borderBottomLeftRadius: 8,
-        }}
-      >
-        <ButtonClasses>
-          <label
-            style={{
-              fontSize: 16,
-              fontWeight: '700',
-              fontFamily: 'Inter',
-              padding: '6px 24px 6px 24px',
-              whiteSpace: 'break-spaces',
-            }}
-          >
-            Hour
-          </label>
-        </ButtonClasses>
-      </div>
-      <Divider
-        type="vertical"
-        style={{
-          marginRight: 0,
-          marginLeft: 0,
-          height: 50,
-          borderColor: '#D3D3D3',
-        }}
-      />
-      <div style={{ background: '#E9E9E9', padding: '4px 0px 4px 0px' }}>
-        <ButtonClasses>
-          <label
-            style={{
-              fontSize: 16,
-              fontWeight: '700',
-              fontFamily: 'Inter',
-              padding: '6px 24px 6px 24px',
-              whiteSpace: 'break-spaces',
-            }}
-          >
-            Day
-          </label>
-        </ButtonClasses>
-      </div>
-      <Divider
-        type="vertical"
-        style={{
-          marginRight: 0,
-          marginLeft: 0,
-          height: 50,
-          borderColor: '#D3D3D3',
-        }}
-      />
-      <div style={{ background: '#E9E9E9', padding: '4px 0px 4px 0px' }}>
-        <ButtonClasses>
-          <label
-            style={{
-              fontSize: 16,
-              fontWeight: '700',
-              fontFamily: 'Inter',
-              padding: '6px 24px 6px 24px',
-              whiteSpace: 'break-spaces',
-            }}
-          >
-            Week
-          </label>
-        </ButtonClasses>
-      </div>
-      <Divider
-        type="vertical"
-        style={{
-          marginRight: 0,
-          marginLeft: 0,
-          height: 50,
-          borderColor: '#D3D3D3',
-        }}
-      />
-      <div
-        style={{
-          background: '#E9E9E9',
-          padding: '4px 4px 4px 0px',
-          borderTopRightRadius: 8,
-          borderBottomRightRadius: 8,
-        }}
-      >
-        <ButtonClasses>
-          <label
-            style={{
-              fontSize: 16,
-              fontWeight: '700',
-              fontFamily: 'Inter',
-              padding: '6px 24px 6px 24px',
-              whiteSpace: 'break-spaces',
-            }}
-          >
-            Month
-          </label>
-        </ButtonClasses>
-      </div>
-    </div>
+    <Container>
+      <BoxTypographyPrice>Price</BoxTypographyPrice>
+      <BoxTypographySelectType>Select Type</BoxTypographySelectType>
+      <BoxContainer>
+        <BoxWrapper>
+          <ButtonClasses>
+            <Label>Hour</Label>
+          </ButtonClasses>
+        </BoxWrapper>
+        <BoxDivider type="vertical" />
+        <BoxDayWeek>
+          <ButtonClasses>
+            <Label>Day</Label>
+          </ButtonClasses>
+        </BoxDayWeek>
+        <BoxDivider type="vertical" />
+        <BoxDayWeek>
+          <ButtonClasses>
+            <Label>Week</Label>
+          </ButtonClasses>
+        </BoxDayWeek>
+        <BoxDivider type="vertical" />
+        <BoxWrrapperRadius>
+          <ButtonClasses>
+            <Label>Month</Label>
+          </ButtonClasses>
+        </BoxWrrapperRadius>
+      </BoxContainer>
 
-    <Typography
-      style={{
-        fontSize: 16,
-        fontFamily: 'Inter',
-        fontWeight: '700',
-        marginTop: 28,
-        marginBottom: 16,
-      }}
-    >
-      Select Type
-    </Typography>
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-      }}
-    >
-      <Dropdown overlay={menu} placement="bottomLeft" trigger={['click']}>
-        <DivPrice>
-          <label
-            style={{ fontFamily: 'Inter', fontSize: 16, fontWeight: '550' }}
-          >
-            $ Any
-          </label>
-          <IconArrowDown />
-        </DivPrice>
-      </Dropdown>
-      <Typography
-        style={{ fontSize: 16, fontFamily: 'Inter', fontWeight: '700' }}
-      >
-        to
-      </Typography>
-      <Dropdown overlay={menu} placement="bottomLeft" trigger={['click']}>
-        <DivPrice>
-          <label
-            style={{ fontFamily: 'Inter', fontSize: 16, fontWeight: '550' }}
-          >
-            $ Any
-          </label>
-          <IconArrowDown />
-        </DivPrice>
-      </Dropdown>
-    </div>
-    <Row gutter={16}>
-      <Col span={12}>
-        <ButtonClear
-          style={{
-            padding: 14,
-            width: '100%',
-            height: 'auto',
-            marginTop: 16,
-            borderRadius: 8,
-          }}
-        >
-          <text
+      <BoxTypographySelectType>Select Type</BoxTypographySelectType>
+      <BoxWrapperTwo>
+        <Dropdown overlay={menu} placement="bottomLeft" trigger={['click']}>
+          <DivPrice>
+            <LabalOne>$ Any</LabalOne>
+            <IconArrowDown />
+          </DivPrice>
+        </Dropdown>
+        <Text>to</Text>
+        <Dropdown overlay={menu} placement="bottomLeft" trigger={['click']}>
+          <DivPrice>
+            <LabalOne>$ Any</LabalOne>
+            <IconArrowDown />
+          </DivPrice>
+        </Dropdown>
+      </BoxWrapperTwo>
+      <Row gutter={16}>
+        <Col span={12}>
+          <ButtonClear
             style={{
-              fontSize: 16,
-              fontWeight: '700',
-              fontFamily: 'Inter',
-              color: 'white',
+              padding: 14,
+              width: '100%',
+              height: 'auto',
+              marginTop: 16,
+              borderRadius: 8,
             }}
           >
-            Clear
-          </text>
-        </ButtonClear>
-      </Col>
-      <Col span={12}>
-        <ButtonApply
-          style={{
-            padding: 14,
-            width: '100%',
-            height: 'auto',
-            marginTop: 16,
-            borderRadius: 8,
-          }}
-        >
-          <text
-            style={{ fontSize: 16, fontWeight: '700', fontFamily: 'Inter' }}
+            <Text>Clear</Text>
+          </ButtonClear>
+        </Col>
+        <Col span={12}>
+          <ButtonApply
+            style={{
+              padding: 14,
+              width: '100%',
+              height: 'auto',
+              marginTop: 16,
+              borderRadius: 8,
+            }}
           >
-            Apply
-          </text>
-        </ButtonApply>
-      </Col>
-    </Row>
-  </div>
-  )
-}
+            <TextOne>Apply</TextOne>
+          </ButtonApply>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default PriceContent
+export default PriceContent;
--- src/components/pages/Search/component/PropertyContent/PropertyContentStyled.tsx
+++ src/components/pages/Search/component/PropertyContent/PropertyContentStyled.tsx
@@ -3,20 +3,31 @@ import { Button, Checkbox, Divider, Image, Row, Typography } from 'antd';
 import { styled } from '@styles/theme';
 
 export const BoxContainer = styled.div`
-  padding: 12;
-  width: 350;
+  padding: 12px;
+  width: 350px;
 `;
 
-export const BoxTypography = styled.div`
-  fontsize: 20;
-  fontfamily: 'Inter';
-  fontweight: '700';
-  marginbottom: 24;
+export const BoxTypography = styled(Typography)`
+  font-size: 20px;
+  font-family: Inter;
+  font-weight: 700;
+  margin-bottom: 24px;
 `;
 
 export const BoxCheckBox = styled(Checkbox)`
-  display: 'flex';
-  align-items: 'center';
-  padding-left: 8px;
+  display: flex;
+  align-items: center;
+  margin-left: 0;
   margin-bottom: 24px;
+  .ant-checkbox-checked .ant-checkbox-inner {
+    background: #f9c41f;
+    border-radius: 4px;
+    color: black;
+    border-color: #f9c41f;
+  }
+  .ant-checkbox-wrapper:hover .ant-checkbox-inner,
+  .ant-checkbox:hover .ant-checkbox-inner,
+  .ant-checkbox-input:focus + .ant-checkbox-inner {
+    border-color: #f9c41f !important;
+  }
 `;
